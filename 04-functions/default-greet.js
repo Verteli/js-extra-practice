96f848e0f92f6c94b4225f504502254c7b9dbc8b
Verteli <123910311+Verteli@users.noreply.github.com>
2023-02-13 04:06:31 -0600

    Update default-greet.js

--- 04-functions/default-greet.js
+++ 04-functions/default-greet.js
@@ -3,6 +3,12 @@
 // If the user does not define a last name, assume the last name is 'Doe'.
 
 // YOUR CODE BELOW
-
+function defaultGreet(first, last){
+    if(last){
+        return 'Hi ' + first + ' ' + last + '!';
+    } else {
+        return 'Hi ' + first + ' Doe!';
+    }
+}
 // Do not change the code below
 module.exports = { defulatGreet };
