0c009619e2dba0c069fbbdd7c158ba911f7a3b01
Verteli <123910311+Verteli@users.noreply.github.com>
2023-02-13 04:07:09 -0600

    Update my-mnemonic.js

--- 04-functions/my-mnemonic.js
+++ 04-functions/my-mnemonic.js
@@ -8,6 +8,24 @@
 // myMnemonic('We', 'Eat', 'Eggs'); // => WEE
 
 // YOUR CODE BELOW
+function myMnemonic (str1, str2, str3, str4){
+    let mnemonic = '';
 
+    if(str1){
+        mnemonic += str1[0];
+    }
+
+    if(str2){
+        mnemonic += str2[0];
+    }
+
+    if(str3){
+        mnemonic += str3[0];
+    }
+
+    if(str4){
+        mnemonic += str4{0};
+    }
+}
 // Do not change the code below this line
 module.exports = { myMnemonic };
